9222423f60c5595e9e70cd90a244bbc01c65668c
root <root@DESKTOP-8CG18A3.localdomain>
2022-01-03 02:33:15 -0300

    task 0 and 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# create a directory including permissions and ownership
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
