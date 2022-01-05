2a1b5c3980e4f56321164e6ec7e7be4b5dcba200
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-04 23:49:47 -0300

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+#Client configuration file (w/ Puppet)
+
+exec { 'echo':
+  path    => '/bin/',
+  command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
+}
