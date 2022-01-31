5717d8a53919206f45a32a35d4fc1302d24a995a
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-31 19:58:39 -0300

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""API"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    users = 'https://jsonplaceholder.typicode.com/users'
+
+    response1 = requests.get(todos)
+    response2 = requests.get(users)
+
+    users_json = response2.json()
+    todos_json = response1.json()
+
+    name = users_json[int(argv[1]) - 1]['username']
+    
+    donetask = 0
+    tasks = 0
+    dosome = []
+    uid = int(argv[1])
+
+    file = open('{}.CSV'.format(uid), 'w')
+
+    for tmp in todos_json:
+        if tmp['userId'] == uid:
+            file.write('"{}","{}","{}","{}"\n'.format(uid, name, tmp['completed'], tmp['title']))
