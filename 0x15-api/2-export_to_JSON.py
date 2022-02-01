3240ad3109d0b2ac5f1259b4ce4a9ea1190014d3
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-31 21:43:01 -0300

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,27 +2,34 @@
 """API"""
 
 
-if __name__ == "__main__":
-    import requests as req
-    from sys import argv
+if __name__ == '__main__':
     import json
-    todos = req.get('https://jsonplaceholder.typicode.com/todos').json()
-    users = req.get('https://jsonplaceholder.typicode.com/users').json()
-    user = {}
-    for elem in users:
-        if elem["id"] == int(argv[1]):
-            user = dict(elem)
-            break
-    user_todos = []
-    for elem in todos:
-        if elem["userId"] == int(argv[1]):
-            user_todos.append(dict(elem))
-    for elem in user_todos:
-        del elem['userId']
-        del elem['id']
-        elem['username'] = user['username']
-        elem['task'] = elem.pop('title')
-    user_todos_export_json = {}
-    user_todos_export_json[argv[1]] = user_todos
-    with open("{}.json".format(argv[1]), "w") as f:
-        json.dump(user_todos_export_json, f)
+    import requests
+    from sys import argv
+
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    users = 'https://jsonplaceholder.typicode.com/users'
+
+    response1 = requests.get(todos)
+    response2 = requests.get(users)
+
+    users_json = response2.json()
+    todos_json = response1.json()
+
+    name = users_json[int(argv[1]) - 1]['username']
+
+    dosome = []
+    uid = argv[1]
+    dic = {}
+    lista = []
+    for tmp in todos_json:
+        if tmp['userId'] == int(uid):
+            tmp['task'] = tmp.pop('title')
+            tmp['username'] = name
+            del tmp['id']
+            del tmp['userId']
+            lista.append(tmp)
+    dic[str(uid)] = lista
+
+    with open('{}.json'.format(argv[1]), 'w') as f:
+        json.dump(dic, f)
