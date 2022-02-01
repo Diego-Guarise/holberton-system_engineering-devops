eb14b4cf462fa5c1063224b2b9a1a437d9071447
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-31 21:29:50 -0300

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""API"""
+
+
+if __name__ == '__main__':
+    import json
+    import requests
+    from sys import argv
+
+    todos = 'https://jsonplaceholder.typicode.com/todos'
+    users = 'https://jsonplaceholder.typicode.com/users'
+
+    response1 = requests.get(todos)
+    response2 = requests.get(users)
+
+    users_json = response2.json()
+    todos_json = response1.json()
+
+    name = users_json[int(argv[1]) - 1]['username']
+
+    dosome = []
+    uid = int(argv[1])
+    dic = {}
+    lista = []
+    for tmp in todos_json:
+        if tmp['userId'] == uid:
+            tmp['task'] = tmp.pop('title')
+            tmp['username'] = name
+            del tmp['id']
+            del tmp['userId']
+            lista.append(tmp)
+    dic[str(uid)] = lista
+
+    with open('{}.json'.format(uid), 'w') as f:
+        json.dump(dic, f)
