6a4e83165baca85a548e7b82005797b73eaca909
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-15 23:56:31 -0300

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Top Ten"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+     function that queries the Reddit API and prints the
+     titles of the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "andydev_"},
+                            allow_redirects=False, params={"limit": 10})
+    if response.status_code == 200:
+        list = response.json().get("data").get("children")
+        for item in list:
+            print(item.get("data").get("title"))
+    else:
+        print("None")
+    
