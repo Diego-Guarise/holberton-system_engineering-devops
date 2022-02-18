a4544145988b1ab95fb9fadb6f089adab4080d4c
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-17 21:54:49 -0300

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Top Ten"""
-
-
+"""
+recursive function that queries the Reddit API and returns a list 
+containing the titles of all hot articles for a given subreddit
+"""
 import requests
 
 
-def top_ten(subreddit):
-    """
-     function that queries the Reddit API and prints the
-     titles of the first 10 hot posts listed for a given subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(url, headers={"User-Agent": "andydev_"},
-                            allow_redirects=False, params={"limit": 10})
-    if response.status_code == 200:
-        list = response.json().get("data").get("children")
-        for item in list:
-            print(item.get("data").get("title"))
+def recurse(subreddit, hot_list=[], pag=""):
+    """asd"""
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = 'https://www.reddit.com/r/' + subreddit +\
+          '/hot.json?after={}'.format(pag)
+    rh = requests.get(url, headers=headers, allow_redirects=False)
+    if rh.status_code != 200:
+        return None
     else:
-        print("None")
+        response = rh.json()
+        tmp = response.get('data').get('children')
+        for data in tmp:
+            hot_list.append(data.get('data').get('title'))
+        pag = response.get('data').get('after')
+        if pag is not None:
+            recurse(subreddit, hot_list, pag)
+        return hot_list
