7b2646cf98156ef94d7638cba42eafbb6ab4ecba
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-24 12:28:09 -0300

    puppet-lint fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,9 @@
 # login with holberton without errors
 exec { 'soft' :
-  command => 'sed -i s/4/4069/ /etc/security/limits.conf',
+  command  => 'sed -i s/4/4069/ /etc/security/limits.conf',
   provider => shell,
 }
 exec { 'hard' :
-  command => 'sed -i s/5/4069/ /etc/security/limits.conf',
+  command  => 'sed -i s/5/4069/ /etc/security/limits.conf',
   provider => shell,
 }
