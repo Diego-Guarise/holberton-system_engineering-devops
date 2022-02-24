2f82a470324a5dc0b98a3f2325cdf03920055748
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-24 12:14:02 -0300

    puppet lint fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # change limit, acept all
 exec { 'change_limit':
-  command => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart',
+  command  => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart',
   provider => shell,
 }
