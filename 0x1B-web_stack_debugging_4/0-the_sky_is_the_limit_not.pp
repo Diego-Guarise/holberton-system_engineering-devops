e1d38c6198dc82c55aba351e5e89277c8c3c8ffa
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-24 12:11:44 -0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,5 @@
 # change limit, acept all
 exec { 'change_limit':
-  command => 'sed -i s/ULIMIT="-n 15"/ULIMIT="-n 4096"/ /etc/default/nginx',
-  path    => '/usr/bin:/bin',
-}
-
-exec { 'restart_nginx':
-  command => 'sudo service nginx restart',
-  path    => ['/usr/bin', '/bin'],
-  require => Exec['change_limit'],
+  command => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart',
+  provider => shell,
 }
